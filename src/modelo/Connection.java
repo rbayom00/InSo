74ae2d9e35043d64e63afe8ce1e79775e4f16ba3
Carball0ULE <55536855+Carball0ULE@users.noreply.github.com>
2021-12-30 18:22:43 +0100

    Connector + Librería actualizada

--- src/modelo/Connection.java
+++ src/modelo/Connection.java
@@ -1,11 +1,41 @@
 package modelo;
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /*
- * Clase encargada de realizar la conexión con la base de datos del proyecto
+ * Clase encargada de realizar la conexión con la base de datos del proyecto.
  * @author Carballo
  */
 
 public class Connection {
-	
+	public static final String user = "VCApp";
+    public static final String pass = "vc@PPV@1d3fR5n0";
+    public static final String host = "localhost";
+    public static final String port = "3306";
+    public static final String db = "";
+    //Url a la conexión de la base de datos con respecto a las vars anteriores
+    public static final String url= "jdbc:mysql://"+host+":"+port+"/"+db;
+    
+    public java.sql.Connection conn;
+
+    public Connection() {
+    	try {
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+    		this.conn = DriverManager.getConnection(url, user, pass);
+    	} catch(ClassNotFoundException err) {
+    		err.printStackTrace();
+    		System.out.println("Connection Error: Class Not Found (MySQL Driver). Check libraries.");
+    	} catch(SQLException err) {
+    		err.printStackTrace();
+    		System.out.println("Connection Error: SQL Error. Check db, login or service.");
+    	}
+    }
+    
+    public java.sql.Connection getConnection() {
+    	return this.conn;
+    }
+    
+    public void disconnect() {
+    	this.conn = null;
+    }
 }
