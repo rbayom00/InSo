a543335d36fa9bb84e82737291957529cc7793aa
rbayom00 <55236794+rbayom00@users.noreply.github.com>
2022-01-04 18:56:56 +0100

    Update test.sql
    cambiados atributos de la tabla usuario para hacer pruebas

--- db/test.sql
+++ db/test.sql
@@ -10,8 +10,9 @@ CREATE TABLE IF NOT EXISTS USUARIOS(
 	DNI CHAR(9) CHECK(DNI LIKE ('[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Z]')),
     constrasena VARCHAR(36) NOT NULL,
     fecha_nacimiento DATE NOT NULL,
-	nombre_completo VARCHAR(255) NOT NULL,
-	nombre_sistema VARCHAR(36),
-    FOREIGN KEY (nombre_sistema) REFERENCES SISTEMA(nombre),
+	nombre VARCHAR(36) NOT NULL,
+	apellidos VARCHAR(255) NOT NULL,
+	domicilio VARCHAR(255) NOT NULL,
+    FOREIGN KEY (nombre) REFERENCES SISTEMA(nombre),
     PRIMARY KEY(DNI)
 );
