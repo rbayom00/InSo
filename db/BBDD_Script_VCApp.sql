b9e8061605a5b0a059d7e303a53ddb4d6ec2d09f
rbayom00 <55236794+rbayom00@users.noreply.github.com>
2022-01-20 20:46:46 +0100

    Update BBDD_Script_VCApp.sql

--- db/BBDD_Script_VCApp.sql
+++ db/BBDD_Script_VCApp.sql
@@ -55,6 +55,9 @@ CREATE TABLE IF NOT EXISTS Tournament(
 	GameDescription VARCHAR(1024) NOT NULL,
 	#'P' for paid, 'F' for free
 	TournamentModality CHAR(1) NOT NULL,
+	Price VARCHAR(6),
+	Prize VARCHAR(1024),
+	Places INT NOT NULL,
     PRIMARY KEY(TournamentID)
 );
 
